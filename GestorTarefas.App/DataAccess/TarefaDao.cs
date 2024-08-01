2478993ee8918d62f153cdf12844ecba854fd4de
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2024-08-01 12:36:00 -0300

    initial commit

---
+++ GestorTarefas.App/DataAccess/TarefaDao.cs
@@ -0,0 +1,47 @@
+﻿using GestorTarefas.App.Interfaces;
+using GestorTarefas.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace GestorTarefas.App.DataAccess
+{
+    public class TarefaDao : ITarefaDao
+    {
+        public void AdicionarTarefa(Tarefa tarefa)
+        {
+            using (var connection = Database.GetConnection())
+            {
+                connection.Open();
+
+                string sql = "INSERT INTO TAREFAS (Nome, Descricao) VALUES (@Nome, @Descricao)";
+                using (var cmd = new SQLiteCommand(sql, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Nome", tarefa.Nome);
+                    cmd.Parameters.AddWithValue("@Descricao", tarefa.Descricao);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        List<Tarefa> ITarefaDao.ObterTarefas()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ITarefaDao.AtualizarTarefa(Tarefa tarefa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletarTarefa(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Tarefa ITarefaDao.ObterTarefaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GestorTarefas.App/Interfaces/ITarefaDao.cs
@@ -0,0 +1,14 @@
+﻿using GestorTarefas.App.Models;
+using System.Collections.Generic;
+
+namespace GestorTarefas.App.Interfaces
+{
+    public interface ITarefaDao
+    {
+        void AdicionarTarefa(Tarefa tarefa);
+        List<Tarefa> ObterTarefas();
+        Tarefa ObterTarefaPorId(int id);
+        void AtualizarTarefa(Tarefa tarefa);
+        void DeletarTarefa(int id);
+    }
+}
